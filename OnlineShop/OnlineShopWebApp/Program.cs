5ab3cef7e6f9b5ab3f5110dfd681d180ad92486b
dels-in <chernenko3954@mail.ru>
2023-09-23 11:10:49 +0300

    поменял .net5.0 на .net7.0 из-за отсутсвия .net5.0 runtime для пк на mac с arm архитектурой

--- OnlineShop/OnlineShopWebApp/Program.cs
+++ OnlineShop/OnlineShopWebApp/Program.cs
@@ -1,26 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace OnlineShopWebApp
-{
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
-	}
-}
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
