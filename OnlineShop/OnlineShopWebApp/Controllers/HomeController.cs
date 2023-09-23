78c5779d8a33c49e357a2ff903b0b9a3e35ad7c0
dels-in <chernenko3954@mail.ru>
2023-09-23 12:55:42 +0300

    добавил в launchSettings.json launch profile OnlineWebShopApp, как на уроке

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string name, int age)
     {
         return View();
     }
--- OnlineShop/OnlineShopWebApp/Program.cs
+++ OnlineShop/OnlineShopWebApp/Program.cs
@@ -1,17 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}");
 
 app.Run();
