1154dc73b1750ce23689b540784b916848a02307
dels-in <chernenko3954@mail.ru>
2023-10-02 12:43:13 +0300

    добавил возможность использования корзины несколькими пользователями

--- OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using ReturnTrue.AspNetCore.Identity.Anonymous;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers;
@@ -8,34 +9,39 @@ public class CartController : Controller
 {
     public IActionResult Index()
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "007";
-        return View(CartStorage.GetAll(userId).OrderBy(p=> p.Name));
+        var feature = HttpContext.Features.Get<IAnonymousIdFeature>();
+        var userId = feature.AnonymousId ?? "007";
+        return View(CartStorage.GetAll(userId).OrderBy(p => p.Name));
     }
 
     public RedirectToActionResult AddToCartRedirect(int id)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)?? "007";
+        var feature = HttpContext.Features.Get<IAnonymousIdFeature>();
+        var userId = feature.AnonymousId ?? "007";
         CartStorage.AddToCart(id, userId);
         return RedirectToAction("Index");
     }
 
     public RedirectToActionResult AddToCartStay(int id)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)?? "007";
+        var feature = HttpContext.Features.Get<IAnonymousIdFeature>();
+        var userId = feature.AnonymousId ?? "007";
         CartStorage.AddToCart(id, userId);
         return RedirectToAction("Index", "Product");
     }
 
     public IActionResult Reduce(int id)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)?? "007";
+        var feature = HttpContext.Features.Get<IAnonymousIdFeature>();
+        var userId = feature.AnonymousId ?? "007";
         CartStorage.Reduce(id, userId);
         return RedirectToAction("Index");
     }
 
     public IActionResult Delete(int id)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)?? "007";
+        var feature = HttpContext.Features.Get<IAnonymousIdFeature>();
+        var userId = feature.AnonymousId ?? "007";
         CartStorage.Delete(id, userId);
         return RedirectToAction("Index");
     }
--- OnlineShop/OnlineShopWebApp/Program.cs
+++ OnlineShop/OnlineShopWebApp/Program.cs
@@ -1,3 +1,5 @@
+using ReturnTrue.AspNetCore.Identity.Anonymous;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
@@ -5,7 +7,7 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 app.UseStaticFiles();
-
+app.UseAnonymousId();
 app.UseRouting();
 
 app.MapControllerRoute(
