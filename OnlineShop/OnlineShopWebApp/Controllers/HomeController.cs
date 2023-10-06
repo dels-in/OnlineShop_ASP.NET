e7ff1ee80b13b37f808f5f2c772ad710d2192c10
dels-in <chernenko3954@mail.ru>
2023-10-06 13:31:12 +0300

    исправил незначительные ошибки

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -21,9 +21,4 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-
-    public IActionResult Search()
-    {
-        return View();
-    }
 }
