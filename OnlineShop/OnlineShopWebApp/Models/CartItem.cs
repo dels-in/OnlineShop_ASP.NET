f5be5cb846475355d9e559b84589eb9fcd937f38
dels-in <chernenko3954@mail.ru>
2023-10-07 13:54:47 +0300

    к предыдущему коммиту

---
+++ OnlineShop/OnlineShopWebApp/Models/CartItem.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models;
+
+public class CartItem
+{
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+
+    public decimal Cost => Product.Cost * Quantity;
+}
