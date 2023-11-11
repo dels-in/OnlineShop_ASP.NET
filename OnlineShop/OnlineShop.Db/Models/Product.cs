ffd4ace9a01854c2d483bf2ec119a40fadba3931
dels-in <chernenko3954@mail.ru>
2023-11-11 14:14:46 +0300

    перенес модель для продукта в проект с БД

---
+++ OnlineShop/OnlineShop.Db/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace OnlineShop.Db.Models;
+
+public class Product
+{
+    public Guid Id { get; set;}
+    public string Name { get; set; }
+    public decimal Cost { get; set; }
+    public string Description { get; set; }
+    public string Source { get; set; }
+    public int MetacriticScore { get; set; }
+    public string Genre { get; set; }
+}
