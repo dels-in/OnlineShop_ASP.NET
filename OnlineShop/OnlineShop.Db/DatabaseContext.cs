53fe85d5fb684a588b7a668ec4f5f1b183724e94
dels-in <chernenko3954@mail.ru>
2023-11-17 22:55:16 +0300

    создал миграцию

--- OnlineShop/OnlineShop.Db/DatabaseContext.cs
+++ OnlineShop/OnlineShop.Db/DatabaseContext.cs
@@ -16,6 +16,6 @@ public class DatabaseContext : DbContext
     public DatabaseContext(DbContextOptions<DatabaseContext> options)
         : base(options)
     {
-        Database.EnsureCreated();
+        Database.Migrate();
     }
 }
