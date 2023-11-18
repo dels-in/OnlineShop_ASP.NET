28de80bcfd4d3b72713d2fa0419276e7e7ded870
dels-in <chernenko3954@mail.ru>
2023-11-18 10:47:14 +0300

    убрал неиспользуемый DbSet<CartItem>

--- OnlineShop/OnlineShop.Db/DatabaseContext.cs
+++ OnlineShop/OnlineShop.Db/DatabaseContext.cs
@@ -7,7 +7,6 @@ public class DatabaseContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Cart> Carts { get; set; }
-    public DbSet<CartItem> CartItems { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<Comparison> Comparisons { get; set; }
     public DbSet<Library> Libraries { get; set; }
