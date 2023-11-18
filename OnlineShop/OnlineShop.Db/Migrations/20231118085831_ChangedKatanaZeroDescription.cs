2260ffa3077a0393296a5dd79792614503be2e2d
dels-in <chernenko3954@mail.ru>
2023-11-18 12:17:25 +0300

    добавил по умолчанию создаваемые продукты

---
+++ OnlineShop/OnlineShop.Db/Migrations/20231118085831_ChangedKatanaZeroDescription.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedKatanaZeroDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Description",
+                value: "Katana ZERO is a stylish neo-noir, platformer featuring breakneck action and death combat. Slash, dash, and manipulate time to unravel your past in a beautifully brutal acrobatic display.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Description",
+                value: "Katana ZERO is a stylish neo-noir, platformer featuring breakneck action and instant-death combat. Slash, dash, and manipulate time to unravel your past in a beautifully brutal acrobatic display.");
+        }
+    }
+}
