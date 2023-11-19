2f12345bb1ce184c7c711f0171fdf2783ab65c82
dels-in <chernenko3954@mail.ru>
2023-11-19 08:34:50 +0300

    добавил роли в бд (2)

---
+++ OnlineShop/OnlineShop.Db/Migrations/20231119053112_AddedRoles.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    RoleName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop.Db/Models/Role.cs
@@ -0,0 +1,7 @@
+namespace OnlineShop.Db.Models;
+
+public class Role
+{
+    public int Id { get; set; }
+    public string RoleName { get; set; }
+}
