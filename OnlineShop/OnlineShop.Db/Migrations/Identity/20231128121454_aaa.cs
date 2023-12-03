e2489b39c2dff13e2214c3c26c3599a1d2ae57c7
dels-in <chernenko3954@mail.ru>
2023-12-03 14:47:45 +0300

    добавил свойства Password и ConfirmPassord, Picture модели User

---
+++ OnlineShop/OnlineShop.Db/Migrations/Identity/20231128121454_aaa.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Db.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class aaa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmPassword",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "AspNetUsers");
+        }
+    }
+}
