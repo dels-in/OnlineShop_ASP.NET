09967d27dc81c9cb6a224d84fc0199f23d7c30a5
dels-in <chernenko3954@mail.ru>
2023-12-03 14:48:20 +0300

    создал класс инициализации Identity

---
+++ OnlineShop/OnlineShop.Db/IdentityInitializer.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Identity;
+using OnlineShop.Db.Helpers;
+using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db;
+
+public class IdentityInitializer
+{
+    public static void Initialize(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        var adminEmail = "admin@mail.ru";
+        var password = "Admin123";
+        if (roleManager.FindByNameAsync("Admin").Result == null)
+        {
+            roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+        }
+
+        if (roleManager.FindByNameAsync("User").Result == null)
+        {
+            roleManager.CreateAsync(new IdentityRole("User")).Wait();
+        }
+
+        if (userManager.FindByNameAsync(adminEmail).Result == null)
+        {
+            var admin = new User { UserName = adminEmail, Email = adminEmail, Password = password.Encrypt(), ConfirmPassword = password.Encrypt() };
+            var result = userManager.CreateAsync(admin, password.Encrypt()).Result;
+            if (result.Succeeded)
+            {
+                userManager.AddToRoleAsync(admin, "Admin").Wait();
+            }
+        }
+    }
+}
