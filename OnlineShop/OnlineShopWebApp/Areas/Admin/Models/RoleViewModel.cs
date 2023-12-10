8dc27a5b1416a90dddb61263e9aa0c989ff6c0e9
dels-in <chernenko3954@mail.ru>
2023-12-10 10:38:52 +0300

    создал модель для работы с ролями вне БД

---
+++ OnlineShop/OnlineShopWebApp/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Areas.Admin.Models;
+
+public class RoleViewModel
+{
+    [Required]
+    public string Name { get; set; }
+}
