cc71672b7043a6d1baee76d37e532463cef5cd57
dels-in <chernenko3954@mail.ru>
2023-12-10 10:39:17 +0300

    добавил методы для маппинга RoleViewModel и IdentityRole

--- OnlineShop/OnlineShopWebApp/Helpers/MappingExtensions.cs
+++ OnlineShop/OnlineShopWebApp/Helpers/MappingExtensions.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using OnlineShop.Db.Helpers;
 using OnlineShop.Db.Models;
+using OnlineShopWebApp.Areas.Admin.Models;
 using OnlineShopWebApp.Models;
 
 namespace OnlineShopWebApp.Helpers;
@@ -16,6 +18,7 @@ public static class MappingExtensions
 
         return userViewModelList;
     }
+
     public static UserViewModel ToUserViewModel(this User user)
     {
         return new UserViewModel
@@ -28,7 +31,7 @@ public static class MappingExtensions
             RoleName = user.RoleName,
         };
     }
-    
+
     public static User ToUser(this UserViewModel userViewModel)
     {
         return new User
@@ -40,6 +43,33 @@ public static class MappingExtensions
             ConfirmPassword = userViewModel.ConfirmPassword.Encrypt(),
             Picture = userViewModel.Picture,
             RoleName = userViewModel.RoleName,
+        };
+    }
+
+    public static List<RoleViewModel> ToRoleViewModelList(this List<IdentityRole> rolesList)
+    {
+        var roleViewModelList = new List<RoleViewModel>();
+        foreach (var role in rolesList)
+        {
+            roleViewModelList.Add(ToRoleViewModel(role));
+        }
+
+        return roleViewModelList;
+    }
+
+    public static RoleViewModel ToRoleViewModel(this IdentityRole role)
+    {
+        return new RoleViewModel
+        {
+            Name = role.Name,
+        };
+    }
+    
+    public static IdentityRole ToRole(this RoleViewModel roleViewModel)
+    {
+        return new IdentityRole
+        {
+            Name = roleViewModel.Name,
         };
     }
 }
