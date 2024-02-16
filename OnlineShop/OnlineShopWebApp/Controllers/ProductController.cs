98b06b45aa005196da751452fe09916a3e9693b7
dels-in <chernenko3954@mail.ru>
2024-02-16 20:23:42 +0300

    изменил контроллер для работы с отзывами

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Db;
 using OnlineShop.Db.Models;
+using OnlineShop.ReviewApi;
+using OnlineShop.ReviewApi.Models;
 using OnlineShopWebApp.Helpers;
 using OnlineShopWebApp.Models;
 
@@ -9,10 +11,12 @@ namespace OnlineShopWebApp.Controllers;
 public class ProductController : Controller
 {
     private readonly IProductStorage _productDbStorage;
+    private readonly ReviewApiClient _reviewApiClient;
 
-    public ProductController(IProductStorage productDbStorage)
+    public ProductController(IProductStorage productDbStorage, ReviewApiClient reviewApiClient)
     {
         _productDbStorage = productDbStorage;
+        _reviewApiClient = reviewApiClient;
     }
 
     public IActionResult Index()
@@ -20,9 +24,20 @@ public class ProductController : Controller
         return View(Mapping<ProductViewModel, Product>.ToViewModelList(_productDbStorage.GetAll()));
     }
 
-    public IActionResult Details(int productId)
+    public async Task<IActionResult> Details(int productId)
     {
         var product = _productDbStorage.GetProduct(productId);
-        return View(Mapping<ProductViewModel, Product>.ToViewModel(product));
+        var productViewModel = Mapping<ProductViewModel, Product>.ToViewModel(product);
+        
+        var reviews = await _reviewApiClient.GetByProductIdAsync(productId);
+        productViewModel.Reviews = Mapping<ReviewViewModel, Review>.ToViewModelList(reviews);
+        
+        return View(productViewModel);
+    }
+
+    public IActionResult AddReview(int productId)
+    {
+        var newReview = new AddReviewViewModel { ProductId = productId };
+        return View();  
     }
 }
