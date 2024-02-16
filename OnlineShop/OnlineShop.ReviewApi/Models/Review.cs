30157c30d269e392afd8c3a8e230236c4ae60152
dels-in <chernenko3954@mail.ru>
2024-02-16 20:24:25 +0300

    модель для отзыва

---
+++ OnlineShop/OnlineShop.ReviewApi/Models/Review.cs
@@ -0,0 +1,34 @@
+namespace OnlineShop.ReviewApi.Models;
+
+public class Review
+{
+    /// <summary>
+    /// Id отзыва
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Id продукта
+    /// </summary>
+    public int ProductId { get; set; }
+
+    /// <summary>
+    /// Id пользователя, оставившего отзыв
+    /// </summary>
+    public int UserId { get; set; }
+
+    /// <summary>
+    /// Текст отзыва
+    /// </summary>
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// Оценка (количество звезд)
+    /// </summary>
+    public int Grade { get; set; }
+
+    /// <summary>
+    /// Дата создания
+    /// </summary>
+    public DateTime CreateDate { get; set; }
+}
